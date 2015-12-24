71e854a2b7a12860f1aaf307ded597a6f9bcff64
hao.f.wang <hao.f.wang@CDC312883.dir.svc.accenture.com>
2015-12-24 15:15:46 +0800

    commit to local & remote

--- MaJiang/src/com/luke/entity/Card.java
+++ MaJiang/src/com/luke/entity/Card.java
@@ -1,5 +1,17 @@
 package com.luke.entity;
 
 public class Card {
+	private int id;
+	
+	public Card(int id){
+		this.setId(id);
+	}
 
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
 }
